fd12afe58507d554c684d88ba9d038e61476e9aa
Regina Wanjiru Kariuki <wanjiruregina@gmail.com>
2025-03-08 21:52:22 +0300

    first class functions

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function receivesAFunction(callback) {
+    callback();
+}
+
+
+function returnsANamedFunction() {
+    return function namedFunction() {
+        console.log("I am a named function!");
+    };
+}
+
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        console.log("I am an anonymous function!");
+    };
+}
+
+module.exports = { receivesAFunction, returnsANamedFunction, returnsAnAnonymousFunction };
+
